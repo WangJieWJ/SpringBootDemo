40b54a6113af6b5a7717bbba7718c79ba806c390
WangJie <1152689483@qq.com>
2017-01-18 14:31:56 +0800

    测试Merge冲突

--- SpringBootTest/src/main/java/com/springBootTest/test/TestDemo.java
+++ SpringBootTest/src/main/java/com/springBootTest/test/TestDemo.java
@@ -17,10 +17,10 @@ import java.util.Map;
  */
 public class TestDemo {
 
-    public static void main(String[] args){
-        String sWgZwidgetJson="[{\"layoutZoneId\":\"2\",\"widgetSourceId\":\"71\",\"widgetSourceName\":\"_6X4a_图文轮播\",\"widgetSourceSize\":\"6X4a\",\"widgetParam\":{\"docId\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"270410\",\"Field\":\"outline\",\"num\":\"4\",\"sourceDocName\":\"天樽3匹柜式空调\"}},\"widgetCss\":{}},{\"layoutZoneId\":\"1\",\"widgetSourceId\":\"94\",\"widgetSourceName\":\"_3*4a单文档图（1:1）文\",\"widgetSourceSize\":\"3X4a\",\"widgetParam\":{\"toplink\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"link\",\"sourceDocName\":\"拯救空气终极计划\"},\"urlpic2\":{\"desc\":\"default-pic2\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"pic2\",\"Relation\":\"toplink\",\"picsize\":\"303\",\"sourceDocName\":\"拯救空气终极计划\"},\"botlink\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"link\",\"sourceDocName\":\"拯救空气终极计划\"},\"title\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"title\",\"Relation\":\"toplink\",\"stringsize\":\"30\",\"sourceDocName\":\"拯救空气终极计划\"},\"constr\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"con\",\"Relation\":\"toplink\",\"sourceDocName\":\"拯救空气终极计划\"},\"con\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"con\",\"Relation\":\"toplink\",\"stringsize\":\"36\",\"sourceDocName\":\"拯救空气终极计划\"}},\"widgetCss\":{}},{\"layoutZoneId\":\"3\",\"widgetSourceId\":\"94\",\"widgetSourceName\":\"_3*4a单文档图（1:1）文\",\"widgetSourceSize\":\"3X4a\",\"widgetParam\":{\"toplink\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"link\",\"sourceDocName\":\"空气净化器百科全书\"},\"urlpic2\":{\"desc\":\"default-pic2\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"pic2\",\"Relation\":\"toplink\",\"picsize\":\"303\",\"sourceDocName\":\"空气净化器百科全书\"},\"botlink\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"link\",\"sourceDocName\":\"空气净化器百科全书\"},\"title\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"title\",\"Relation\":\"toplink\",\"stringsize\":\"30\",\"sourceDocName\":\"空气净化器百科全书\"},\"constr\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"con\",\"Relation\":\"toplink\",\"sourceDocName\":\"空气净化器百科全书\"},\"con\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"con\",\"Relation\":\"toplink\",\"stringsize\":\"36\",\"sourceDocName\":\"空气净化器百科全书\"}},\"widgetCss\":{}}]";
+    public static void main(String[] args) {
+        String sWgZwidgetJson = "[{\"layoutZoneId\":\"2\",\"widgetSourceId\":\"71\",\"widgetSourceName\":\"_6X4a_图文轮播\",\"widgetSourceSize\":\"6X4a\",\"widgetParam\":{\"docId\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"270410\",\"Field\":\"outline\",\"num\":\"4\",\"sourceDocName\":\"天樽3匹柜式空调\"}},\"widgetCss\":{}},{\"layoutZoneId\":\"1\",\"widgetSourceId\":\"94\",\"widgetSourceName\":\"_3*4a单文档图（1:1）文\",\"widgetSourceSize\":\"3X4a\",\"widgetParam\":{\"toplink\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"link\",\"sourceDocName\":\"拯救空气终极计划\"},\"urlpic2\":{\"desc\":\"default-pic2\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"pic2\",\"Relation\":\"toplink\",\"picsize\":\"303\",\"sourceDocName\":\"拯救空气终极计划\"},\"botlink\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"link\",\"sourceDocName\":\"拯救空气终极计划\"},\"title\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"title\",\"Relation\":\"toplink\",\"stringsize\":\"30\",\"sourceDocName\":\"拯救空气终极计划\"},\"constr\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"con\",\"Relation\":\"toplink\",\"sourceDocName\":\"拯救空气终极计划\"},\"con\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283290\",\"Field\":\"con\",\"Relation\":\"toplink\",\"stringsize\":\"36\",\"sourceDocName\":\"拯救空气终极计划\"}},\"widgetCss\":{}},{\"layoutZoneId\":\"3\",\"widgetSourceId\":\"94\",\"widgetSourceName\":\"_3*4a单文档图（1:1）文\",\"widgetSourceSize\":\"3X4a\",\"widgetParam\":{\"toplink\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"link\",\"sourceDocName\":\"空气净化器百科全书\"},\"urlpic2\":{\"desc\":\"default-pic2\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"pic2\",\"Relation\":\"toplink\",\"picsize\":\"303\",\"sourceDocName\":\"空气净化器百科全书\"},\"botlink\":{\"desc\":\"资源ID\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"link\",\"sourceDocName\":\"空气净化器百科全书\"},\"title\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"title\",\"Relation\":\"toplink\",\"stringsize\":\"30\",\"sourceDocName\":\"空气净化器百科全书\"},\"constr\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"con\",\"Relation\":\"toplink\",\"sourceDocName\":\"空气净化器百科全书\"},\"con\":{\"desc\":\"default-title\",\"type\":\"MetaViewData\",\"value\":\"283289\",\"Field\":\"con\",\"Relation\":\"toplink\",\"stringsize\":\"36\",\"sourceDocName\":\"空气净化器百科全书\"}},\"widgetCss\":{}}]";
 
-        String sLayoutHtml="<div class=\"hn_interaction_content pure-g\">\n" +
+        String sLayoutHtml = "<div class=\"hn_interaction_content pure-g\">\n" +
                 "                <div class=\"pure-u-1-4 h-1-1\">\n" +
                 "                   <div class=\"l-box\">\n" +
                 "                     @WidgetLayout{\"layoutid\":\"1\",\"desc\":\"3X4a区域一\",\"sourcesize\":\"3X4a\"}@WidgetLayout\n" +
@@ -40,32 +40,32 @@ public class TestDemo {
 
         String[] sLayoutHtmlArr = sLayoutHtml.split("@WidgetLayout"); //肯定会出现一个
 
-        for(String s:sLayoutHtmlArr) {
+        for (String s : sLayoutHtmlArr) {
             System.out.println("---------------------------------------------");
             System.out.println(s);
         }
 
         //jsonArray 使用的是Widget资源的关联关系JSON转换而来
-        if(sLayoutHtmlArr.length>0){
-            for (int i = 0  ; i < sLayoutHtmlArr.length; i++) {
-                if(i>0&&i%2!=0){
+        if (sLayoutHtmlArr.length > 0) {
+            for (int i = 0; i < sLayoutHtmlArr.length; i++) {
+                if (i > 0 && i % 2 != 0) {
                     System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
                     System.out.println(sLayoutHtmlArr[i]);
                     //logger.info("print to fufeiwu WidgetLayout新 "+"@WidgetLayout"+sLayoutHtmlArr[i]+"@WidgetLayout"+" y "+resultHtml+" z "+g_scretime);
-                    String shtmlRe = ("@WidgetLayout"+sLayoutHtmlArr[i]+"@WidgetLayout").replaceAll("\\{","").replaceAll("\\}", "");
-                    sLayoutHtml = sLayoutHtml.replaceAll("\\{","").replaceAll("\\}", "");
+                    String shtmlRe = ("@WidgetLayout" + sLayoutHtmlArr[i] + "@WidgetLayout").replaceAll("\\{", "").replaceAll("\\}", "");
+                    sLayoutHtml = sLayoutHtml.replaceAll("\\{", "").replaceAll("\\}", "");
                 }
             }
         }
 
 
-        String[] Str=new String[]{"Str"};
+        String[] Str = new String[]{"Str"};
         System.out.println(Str[0]);
         System.out.println(Str.length);
 
         System.out.println("000000000000000000000000000");
 
-        List<Integer> list=new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(3);
         list.add(6);
@@ -73,18 +73,18 @@ public class TestDemo {
         System.out.println(list.contains(1));
         System.out.println(list.contains(6));
 
-        for(int i=0;i<9;i++){
-            for(int j=0;j<8;j++){
-                if(j>=4){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (j >= 4) {
                     System.out.println("888888888888888888888888");
                     break;
                 }
-                System.out.println(j+"&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+                System.out.println(j + "&&&&&&&&&&&&&&&&&&&&&&&&&&&");
             }
-            System.out.println(i+"-------------");
+            System.out.println(i + "-------------");
         }
 
-        String DeStr="\n" +
+        String DeStr = "\n" +
                 "<div class=\"hn_interaction_content pure-g\">\n" +
                 "    <div class=\"pure-u-1-2 h-1-1\">\n" +
                 "        <div class=\"l-box\">\n" +
@@ -129,15 +129,15 @@ public class TestDemo {
                 "    </div>\n" +
                 "</div>";
 
-        String [] strArray=DeStr.split("@WidgetLayout");
-        for(String s:strArray) {
+        String[] strArray = DeStr.split("@WidgetLayout");
+        for (String s : strArray) {
             System.out.println(s);
             System.out.println("*****************************");
         }
 
         String shtmlRe = ("@WidgetLayout" + sLayoutHtmlArr[3] + "@WidgetLayout").replaceAll("\\{", "").replaceAll("\\}", "");
         DeStr = DeStr.replaceAll("\\{", "").replaceAll("\\}", "");
-        System.out.println(shtmlRe+"------------");
+        System.out.println(shtmlRe + "------------");
         DeStr = DeStr.replace(shtmlRe, "asdadsdsad"); //All
 
         System.out.println(DeStr);
@@ -146,51 +146,49 @@ public class TestDemo {
 
         try {
 
-            String HTML="%E6%B5%8B%E8%AF%95%E4%B8%AD%E6%96%87%E4%B9%B1%E7%A0%81";
-            System.out.println(URLDecoder.decode(HTML,"UTF-8"));
-            System.out.println(URLDecoder.decode(HTML,"GB2312"));
-            String HTMLT=URLDecoder.decode(HTML);
+            String HTML = "%E6%B5%8B%E8%AF%95%E4%B8%AD%E6%96%87%E4%B9%B1%E7%A0%81";
+            System.out.println(URLDecoder.decode(HTML, "UTF-8"));
+            System.out.println(URLDecoder.decode(HTML, "GB2312"));
+            String HTMLT = URLDecoder.decode(HTML);
 
-            System.out.println("默认编码方式："+Charset.defaultCharset());
-            String HTML11="中文乱码测试";
-            System.out.println(new String(HTMLT.getBytes(),Charset.forName("gb2312")));
+            System.out.println("默认编码方式：" + Charset.defaultCharset());
+            String HTML11 = "中文乱码测试";
+            System.out.println(new String(HTMLT.getBytes(), Charset.forName("gb2312")));
 
             //将文字从utf-8转换为gb2312
             System.out.println("将文字从utf-8转换为gb2312");
-            byte[] HTML11Bytes=HTML11.getBytes("UTF-8");
-            String newHTML11Bytes = new String(HTML11Bytes,Charset.forName("GBK"));
+            byte[] HTML11Bytes = HTML11.getBytes("UTF-8");
+            String newHTML11Bytes = new String(HTML11Bytes, Charset.forName("GBK"));
             System.out.println(newHTML11Bytes);
 
-            byte[] Temp=DeStr.getBytes("UTF-8");
-            String Temp1=new String(Temp,Charset.forName("GBK"));
+            byte[] Temp = DeStr.getBytes("UTF-8");
+            String Temp1 = new String(Temp, Charset.forName("GBK"));
             System.out.println(Temp1);
 
             System.out.println("将文字从GB2312转为UTF-8");
-            byte[] GB2312=newHTML11Bytes.getBytes("gb2312");
-            String newGB2312=new String(GB2312,"gb2312");
+            byte[] GB2312 = newHTML11Bytes.getBytes("gb2312");
+            String newGB2312 = new String(GB2312, "gb2312");
             System.out.println(newGB2312);
 
 
             System.out.println("解码编码测试");
-            String _str="中文乱码测试";
-            String _str1= URLEncoder.encode(_str,"GBK");
+            String _str = "中文乱码测试";
+            String _str1 = URLEncoder.encode(_str, "GBK");
             System.out.println("使用GBK编码结果：" + _str1);
-            String _str2=URLDecoder.decode(_str1,"GBK");
+            String _str2 = URLDecoder.decode(_str1, "GBK");
             System.out.println("使用GBK解码结果：" + _str2);
 
-            String _Str="中文乱码测试结果";
-            String _Str1=URLEncoder.encode(_Str,"UTF-8");
+            String _Str = "中文乱码测试结果";
+            String _Str1 = URLEncoder.encode(_Str, "UTF-8");
             System.out.println("使用UTF-8编码结果：" + _Str);
-            String _Str2=URLDecoder.decode(_Str1,"UTF-8");
+            String _Str2 = URLDecoder.decode(_Str1, "UTF-8");
             System.out.println("使用UTF-8解码结果：" + _Str2);
 
             System.out.println("当前正在dev分支，测试冲突！！！");
-            System.out.println("当前正在dev分支，测试冲突！！！");
-
-
+            System.out.println("Hello World Demo ");
             System.out.println();
 
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
