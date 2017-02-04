b8c5acc50f7b9d27374a4aec10af02c8271909f9
WangJie <1152689483@qq.com>
2017-02-04 14:51:27 +0800

    项目完善

--- SpringBootBasic/src/main/java/com/springBootBasic/SpringBootApp.java
+++ SpringBootBasic/src/main/java/com/springBootBasic/SpringBootApp.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * Title: SpringBoot启动类
  * Description:
@@ -17,10 +22,25 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SpringBootApp {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        SpringApplication.run(SpringBootApp.class,args);
-        SpringApplication app=new SpringApplication(SpringBootApp.class);
+        SpringApplication app = new SpringApplication(SpringBootApp.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
+    }
+
+    //对象的深拷贝
+    public Object deepClone() throws Exception {
+        // 序列化
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+
+        oos.writeObject(this);
+
+        // 反序列化
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+
+        return ois.readObject();
     }
 }
--- SpringBootBasic/src/main/java/com/springBootBasic/controller/TestController.java
+++ SpringBootBasic/src/main/java/com/springBootBasic/controller/TestController.java
@@ -3,8 +3,7 @@ package com.springBootBasic.controller;
 import com.springBootBasic.pojo.AuthorSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Title: 测试Controller
@@ -33,5 +32,13 @@ public class TestController {
     @RequestMapping("/index1")
     public String getStr1(){
         return "使用类型安全的配置：author name is "+authorSettings.getName()+" and author age is "+authorSettings.getAge();
+    }
+
+
+    //规定请求的方式只能是POST请求
+    //规定参数Username为必传参数(RequestParam)，id和age为可选
+    @RequestMapping(value="/login",method= RequestMethod.POST)
+    public String loginPost(@RequestParam String Username,Long id,Integer age){
+        return Username+" POST方式登录成功！！！ id:"+id+" age"+age;
     }
 }
