64b6e9ffafb1ffe55ad94b10340eaa42e9fce507
wangjie <1152689483@qq.com>
2017-08-04 20:30:36 +0800

    按行读取日志文件中的日志(筛选日志文件)

--- SpringBootCommon/src/main/java/com/springboot/common/StrUtil.java
+++ SpringBootCommon/src/main/java/com/springboot/common/StrUtil.java
@@ -1,8 +1,6 @@
 package com.springboot.common;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 
 /**
  * Title: 字符串工具类
@@ -20,30 +18,30 @@ public class StrUtil {
     }
 
     public static Integer toSafeInteger(Object object) {
-        String Str=toSafeString(object);
+        String Str = toSafeString(object);
         if (isEmpty(Str)) {
             return null;
         }
         return Integer.getInteger(Str); //如果发生格式转换失败的问题，就会返回null
     }
 
-    public static Long toSafeLong(Object object){
-        String Str=toSafeString(object);
-        if(isEmpty(Str)){
+    public static Long toSafeLong(Object object) {
+        String Str = toSafeString(object);
+        if (isEmpty(Str)) {
             return null;
         }
         return Long.getLong(Str); //如果发生格式转换失败的问题，就会返回null
     }
 
-    public static Double toSafeDouble(Object object){
-        String Str=toSafeString(object);
-        if(isEmpty(Str)){
+    public static Double toSafeDouble(Object object) {
+        String Str = toSafeString(object);
+        if (isEmpty(Str)) {
             return null;
         }
         try {
-            Double returnDouble=Double.parseDouble(Str);
+            Double returnDouble = Double.parseDouble(Str);
             return returnDouble;
-        }catch (NumberFormatException ex){
+        } catch (NumberFormatException ex) {
             return null;
         }
     }
@@ -59,16 +57,38 @@ public class StrUtil {
 
         StringBuffer out = new StringBuffer();
         byte[] b = new byte[4096];
-        for (int n; (n = in.read(b)) != -1;) {
+        for (int n; (n = in.read(b)) != -1; ) {
             out.append(new String(b, 0, n, encode));
         }
         return out.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(toSafeString(null).equals(""));
-        System.out.println(toSafeDouble("jk"));
-        System.out.println(toSafeInteger("1232"));
+        //读取日志文件，并进行筛选
+        StringBuffer stringBuffer = new StringBuffer();
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader("I:\\日志文件\\hyb.out"));
+            String str = null;
+            while ((str = bufferedReader.readLine()) != null) {
+                if (str.indexOf("来自haierBBS的【非动态】短信登陆") > 0) {
+                    stringBuffer.append(str + "\n");
+                }
+            }
+
+            bufferedWriter = new BufferedWriter(new FileWriter("I:\\日志文件\\Filter_haierBBS_非动态.txt"));
+            bufferedWriter.write(stringBuffer.toString());
+        } catch (IOException e) {
+            try {
+                bufferedReader.close();
+                bufferedWriter.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+
 
     }
 
