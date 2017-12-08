b89638aea497e85e180471039b84f46b15f96f68
wangjie <1152689483@qq.com>
2017-12-08 17:51:49 +0800

    钉钉机器人测试

--- SpringBootBasic/src/main/java/com/springbootbasic/aspect/ControllerAspect.java
+++ SpringBootBasic/src/main/java/com/springbootbasic/aspect/ControllerAspect.java
@@ -7,7 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
- * Title:
+ * Title: ControllerAspect修改测试
  * Description:
  * Copyright: 2017 北京拓尔思信息技术股份有限公司 版权所有.保留所有权
  * Company:北京拓尔思信息技术股份有限公司(TRS)
